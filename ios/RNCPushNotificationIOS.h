37f963dfb976037336375b26d453c95b539b79aa
Rafael Lincoln <rafaellincoln@enesolucoes.com.br>
2019-02-12 05:43:18 -0200

    Migrate the PushNotificationIOS module from React Native core to the community repo

---
+++ ios/RNCPushNotificationIOS.h
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#import <React/RCTEventEmitter.h>
+
+extern NSString *const RNCRemoteNotificationReceived;
+
+@interface RNCPushNotificationIOS : RCTEventEmitter
+
+typedef void (^RNCRemoteNotificationCallback)(UIBackgroundFetchResult result);
+
+#if !TARGET_OS_TV
++ (void)didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings;
++ (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
++ (void)didReceiveRemoteNotification:(NSDictionary *)notification;
++ (void)didReceiveRemoteNotification:(NSDictionary *)notification fetchCompletionHandler:(RNCRemoteNotificationCallback)completionHandler;
++ (void)didReceiveLocalNotification:(UILocalNotification *)notification;
++ (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
+#endif
+
+@end
