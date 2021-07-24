bda691d7f5ae66fbe9e082f97cc508c5e994f647
AP Fritts <2000021+apfritts@users.noreply.github.com>
2021-07-24 16:48:50 -0700

    Cleanup and guard against missing notificationIds -- like if finish is called twice

--- ios/RNCPushNotificationIOS.m
+++ ios/RNCPushNotificationIOS.m
@@ -110,7 +110,6 @@ RCT_EXPORT_MODULE()
               fetchCompletionHandler:(RNCRemoteNotificationCallback)completionHandler
 {
   NSDictionary *userInfo = @{@"notification": notification, @"completionHandler": completionHandler};
-  NSLog(@"NOTIFICATION: RNCPushNotification didReceiveRemoteNotification called");
   [[NSNotificationCenter defaultCenter] postNotificationName:RCTRemoteNotificationReceived
                                                       object:self
                                                     userInfo:userInfo];
@@ -142,9 +141,7 @@ API_AVAILABLE(ios(10.0)) {
   NSString *notificationId = [[NSUUID UUID] UUIDString];
   remoteNotification[@"notificationId"] = notificationId;
   remoteNotification[@"remote"] = @YES;
-  NSLog(@"NOTIFICATION: RNCPushNotification handleRemoteNotificationReceived called");
   if (completionHandler) {
-    NSLog(@"NOTIFICATION: has completion handler");
     if (!self.remoteNotificationCallbacks) {
       // Lazy initialization
       self.remoteNotificationCallbacks = [NSMutableDictionary dictionary];
@@ -173,11 +170,11 @@ API_AVAILABLE(ios(10.0)) {
 
 RCT_EXPORT_METHOD(onFinishRemoteNotification:(NSString *)notificationId fetchResult:(UIBackgroundFetchResult)result)
 {
-  NSLog(@"NOTIFICATION: RNCPushNotification onFinishRemoteNotification called");
   if (self.remoteNotificationCallbacks) {
-    NSLog(@"NOTIFICATION: RNCPushNotification has callbacks");
-    RNCRemoteNotificationCallback completionHandler = self.remoteNotificationCallbacks[notificationId];
-    completionHandler(result);
+    RNCRemoteNotificationCallback completionHandler = [self.remoteNotificationCallbacks objectForKey:notificationId]
+    if (completionHandler) {
+      completionHandler(result);
+    }
     [self.remoteNotificationCallbacks removeObjectForKey:notificationId];
   }
 }
