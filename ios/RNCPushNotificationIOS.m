199d4c327d8a9a0c88209be226e06a59bc0fbbc3
AP Fritts <2000021+apfritts@users.noreply.github.com>
2021-07-24 14:15:35 -0700

    Attempting to fix completion handler bug

--- ios/RNCPushNotificationIOS.m
+++ ios/RNCPushNotificationIOS.m
@@ -110,6 +110,7 @@ RCT_EXPORT_MODULE()
               fetchCompletionHandler:(RNCRemoteNotificationCallback)completionHandler
 {
   NSDictionary *userInfo = @{@"notification": notification, @"completionHandler": completionHandler};
+  NSLog(@"NOTIFICATION: RNCPushNotification didReceiveRemoteNotification called")
   [[NSNotificationCenter defaultCenter] postNotificationName:RCTRemoteNotificationReceived
                                                       object:self
                                                     userInfo:userInfo];
@@ -141,7 +142,9 @@ API_AVAILABLE(ios(10.0)) {
   NSString *notificationId = [[NSUUID UUID] UUIDString];
   remoteNotification[@"notificationId"] = notificationId;
   remoteNotification[@"remote"] = @YES;
+  NSLog(@"NOTIFICATION: RNCPushNotification handleRemoteNotificationReceived called")
   if (completionHandler) {
+    NSLog(@"NOTIFICATION: has completion handler")
     if (!self.remoteNotificationCallbacks) {
       // Lazy initialization
       self.remoteNotificationCallbacks = [NSMutableDictionary dictionary];
@@ -170,7 +173,13 @@ API_AVAILABLE(ios(10.0)) {
 
 RCT_EXPORT_METHOD(onFinishRemoteNotification:(NSString *)notificationId fetchResult:(UIBackgroundFetchResult)result)
 {
-  [self.remoteNotificationCallbacks removeObjectForKey:notificationId];
+  NSLog(@"NOTIFICATION: RNCPushNotification onFinishRemoteNotification called")
+  if (self.remoteNotificationCallbacks) {
+    NSLog(@"NOTIFICATION: RNCPushNotification has callbacks")
+    RNCRemoteNotificationCallback completionHandler = self.remoteNotificationCallbacks[notificationId]
+    completionHandler(result)
+    [self.remoteNotificationCallbacks removeObjectForKey:notificationId];
+  }
 }
 
 /**
